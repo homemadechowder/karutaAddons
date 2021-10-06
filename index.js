acc6cdbb4555a010cb3a666431f82cfc24135729
Andrew Lu <andrew@cohome.com>
2021-10-05 19:49:49 -0700

    add image

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const setCurrentCard = (msg) => {
   return;
 };
 
-const karutaBurnHandler = (tag, message, ed) => {
+const karutaBurnHandler = (tag, message, ed, currUser) => {
   if (message.embeds[0]?.title === "Card Collection") {
     const currentTag = tag === "" ? "burn" : tag;
     const cards = message.embeds[0].description;
@@ -38,11 +38,21 @@ const karutaBurnHandler = (tag, message, ed) => {
         .setLabel("Copy")
         .setStyle("PRIMARY")
     );
+    console.log(currUser);
 
     const embed = new MessageEmbed()
-      .setColor("#0099ff")
-      .setTitle("Karuta Tag")
-      .setDescription("```" + karutaBurn(currentTag, cards, ed) + "```");
+      .setColor("#E97451")
+      .setAuthor("Karuta Multi Tag", "https://i.imgur.com/F3yStSS.png")
+      .setDescription(
+        `<@${currUser.id}>, here is your Karuta Code\n` +
+          "```" +
+          karutaBurn(currentTag, cards, ed) +
+          "```"
+      )
+      .setThumbnail(
+        `https://cdn.discordapp.com/avatars/${currUser.id}/${currUser.avatar}.png`
+      )
+      .setImage("https://i.imgur.com/cRaaTrl.png");
 
     message.channel.send({
       content: " ",
@@ -56,6 +66,7 @@ const karutaBurnHandler = (tag, message, ed) => {
 
 bot.on("ready", () => {
   console.info(`Logged in as ${bot.user.tag}!`);
+  bot.user.setUsername("Karuta Multi Tag");
   // bot.channels.get('875133028588982283').send('bruh');
 });
 
@@ -93,6 +104,7 @@ bot.on("messageCreate", async (msg) => {
     }
     const tag = parsed.arguments[0];
     const ed = parsed.arguments[1];
-    karutaBurnHandler(tag || "burn", currentCard, ed);
+
+    karutaBurnHandler(tag || "burn", currentCard, ed, msg.author);
   }
 });
